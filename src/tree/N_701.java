3d50a56b8f15c40286650116b64569fc5e3ad473
Cyan99 <739092814@qq.com>
2022-03-28 12:09:45 +0800

    在二叉搜索树中插入节点，利用它的有序性质，迭代方法要存它的上一个节点

---
+++ src/tree/N_701.java
@@ -0,0 +1,39 @@
+package tree;
+
+public class N_701 {
+    //递归插入，遇到空节点就按二叉搜索树的规则插入
+//    public TreeNode insertIntoBST(TreeNode root, int val) {
+//        if(root == null){
+//            return new TreeNode(val);
+//        }
+//        if(root.val > val){
+//            root.left = insertIntoBST(root.left, val);
+//        }
+//        if(root.val < val){
+//            root.right = insertIntoBST(root.right, val);
+//        }
+//        return root;
+//    }
+    //迭代插入
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode (val);
+        }
+        TreeNode cur = root;
+        TreeNode pre = root;
+        while (cur != null) {
+            pre = cur;
+            if (cur.val > val) {
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+        if (pre.val > val) {
+            pre.left = new TreeNode(val);
+        } else {
+            pre.right = new TreeNode(val);
+        }
+        return root;
+    }
+}
